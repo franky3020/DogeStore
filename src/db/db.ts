66e5d46408bc013a191621171cdc5696114e0ea9
franky3020 <210238hwsh@gmail.com>
2022-05-27 11:48:58 +0800

    add jest

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,6 +1,6 @@
 import mysql from "mysql2";
 
-export const connection = mysql.createConnection({
+export const connection = mysql.createConnection({ // 要注意connection 拿著太久時 會出錯
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PWD,
@@ -25,7 +25,6 @@ export function createNewDatabase(databaseName: string): Promise<mysql.Connectio
 
       connection.execute("CREATE DATABASE IF NOT EXISTS " + databaseName, function (err, result) {
         if (err) throw err;
-
 
         let newConnection: mysql.Connection = mysql.createConnection({
           host: process.env.DB_HOST,
@@ -60,9 +59,6 @@ export function deletesDatabase(databaseName: string): Promise<void> { // return
 
       connection.execute("DROP DATABASE IF EXISTS " + databaseName , function (err, result) {
         if (err) throw err;
-
-
-        
         resolve();
       });
 
