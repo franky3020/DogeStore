0fe3f315c4e76e497198f856a0ad040b86fa7b9a
franky3020 <210238hwsh@gmail.com>
2022-05-30 00:15:00 +0800

    完成3個 產品的API

--- src/index.ts
+++ src/index.ts
@@ -6,3 +6,50 @@
 // });
 
 // connection.end();
+
+import express, { Express, Request, Response, NextFunction } from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+
+import ProductCtrl from "./controller/ProductCtrl";
+import {initAlltables, insertFakeData} from "./db/seed";
+
+const app: Express = express();
+app.use(express.json());
+
+const port = process.env.SERVER_PORT;
+
+import { validate, ValidationError, Joi } from "express-validation";
+
+const productValidation = {
+    body: Joi.object({
+      name: Joi.string()
+        .required()
+    }),
+  }
+
+app.get('/product/:id', ProductCtrl.getProductById);
+app.get('/product', ProductCtrl.getAllProduct);
+app.post('/product', validate(productValidation), ProductCtrl.addNewProduct);
+
+// 有4個參數的方法 一定是用來做錯誤處理的
+app.use(function( err: any, req: Request, res: Response, next: NextFunction) {
+    if (err instanceof ValidationError) {
+        return res.status(err.statusCode).json(err)
+    }
+
+    return res.status(500).json(err)
+})
+
+//The 404 Route (ALWAYS Keep this as the last route)
+app.get('*', function(req, res){
+    res.status(404).send('nothing');
+});
+
+
+app.listen(port, async () => {
+
+  await initAlltables(process.env.DB_NAME as string);
+  await insertFakeData(process.env.DB_NAME as string);
+  console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
+});
